61b60b6991902f975198d108b81c32a76ac241fe
Esteeban <erojas@utem.cl>
2021-11-11 11:56:29 -0300

    Agregando dependencias Firebase

--- appolmedo/lib/main.dart
+++ appolmedo/lib/main.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:image_picker/image_picker.dart';
 import 'package:appolmedo/pages/admin_pages.dart';
 import 'package:appolmedo/pages/chofer_pages.dart';
 import 'package:appolmedo/pages/olvido_contrase%C3%B1a.dart';
