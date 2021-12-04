3f9ff9f157c75f62e7db01d024c63c33f0d633dc
DiegoN231197 <diego.nunezg@utem.cl>
2021-12-03 22:45:00 -0300

    Icono, splash screen y cambios

---
+++ appolmedo/lib/src/pages/cerrarsesion.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:appolmedo/src/pages/chofer_pages.dart';
+import 'package:appolmedo/src/pages/selectCamion.dart';
+
+class Cerrarsesion extends StatefulWidget {
+  Cerrarsesion({Key? key}) : super(key: key);
+
+  @override
+  _CerrarsesionState createState() => _CerrarsesionState();
+}
+
+class _CerrarsesionState extends State<Cerrarsesion> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        backgroundColor: Colors.orange[600],
+        title: Text('Cerrar sesión'),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(horizontal: 35),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              const SizedBox(
+                height: 20,
+              ),
+              //casilla fecha de entrega
+              TextFormField(
+                decoration: const InputDecoration(
+                  border: UnderlineInputBorder(),
+                  filled: true,
+                  icon: Icon(
+                    Icons.date_range,
+                    color: Colors.black,
+                  ),
+                  labelText: "Fecha de entrega",
+                ),
+                initialValue: fechaActual(),
+                keyboardType: TextInputType.datetime,
+              ),
+              const SizedBox(height: 20),
+              //casilla hora de entrega
+              TextFormField(
+                decoration: const InputDecoration(
+                  border: UnderlineInputBorder(),
+                  filled: true,
+                  icon: Icon(
+                    Icons.timer,
+                    color: Colors.black,
+                  ),
+                  labelText: "Hora de entrega",
+                ),
+                initialValue: horaActual(),
+                keyboardType: TextInputType.datetime,
+              ),
+
+              //casilla odometro
+              const SizedBox(height: 20),
+              TextFormField(
+                decoration: const InputDecoration(
+                  border: UnderlineInputBorder(),
+                  filled: true,
+                  icon: Icon(
+                    Icons.local_shipping,
+                    color: Colors.black,
+                  ),
+                  labelText: "Odometro final del camión",
+                ),
+              ),
+              const SizedBox(height: 20),
+              mensajeDetalle(),
+              const SizedBox(height: 20),
+              new MaterialButton(
+                onPressed: () {
+                  Navigator.pushReplacementNamed(context, '/LoginPage');
+                },
+                height: 40,
+                minWidth: 60,
+                child: Text("Enviar y cerrar sesión",
+                    style: TextStyle(fontSize: 20)),
+                color: Colors.orange[600],
+                splashColor: Colors.blue,
+                textColor: Colors.white,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  final detallecontroller = TextEditingController();
+  Widget mensajeDetalle() {
+    return TextField(
+      controller: this.detallecontroller,
+      maxLines: 6,
+      textCapitalization: TextCapitalization.sentences,
+      decoration: InputDecoration(
+        counterText: '${this.detallecontroller.text.split('').length} words',
+        border: const OutlineInputBorder(),
+        hintText: "Detalles de ruta",
+      ),
+      onChanged: (text) => setState(() {}),
+    );
+  }
+
+  String fechaActual() {
+    var now = new DateTime.now();
+    var dato = (now.day.toString() +
+        "/" +
+        now.month.toString() +
+        "/" +
+        now.year.toString());
+    return dato;
+  }
+
+  String horaActual() {
+    var now = new DateTime.now();
+    var hora = (now.hour.toString() + ":" + now.minute.toString());
+    return hora;
+  }
+}
